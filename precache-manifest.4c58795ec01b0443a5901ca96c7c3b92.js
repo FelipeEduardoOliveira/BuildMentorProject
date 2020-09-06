679acbb25dd2439f7d2fd0034beeaa118b4cfda5
Felipe Oliveira <feoliveira.eduardo@gmail.comgit config --global user.email feoliveira.eduardo@gmail.comgit config --global user.email feoliveira.eduardo@gmail.com>
2020-09-05 21:44:30 -0300

    Build

---
+++ precache-manifest.4c58795ec01b0443a5901ca96c7c3b92.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a6e2236effbac0685060342947bc182",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ad152b8edf9152ff844",
+    "url": "/static/css/main.06f74067.chunk.css"
+  },
+  {
+    "revision": "dd99b21a5f4db01581c5",
+    "url": "/static/js/2.9f2adbe7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9f2adbe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ad152b8edf9152ff844",
+    "url": "/static/js/main.5a0989d2.chunk.js"
+  },
+  {
+    "revision": "a7b5990e52f36fac942e",
+    "url": "/static/js/runtime-main.4044ea59.js"
+  },
+  {
+    "revision": "88090acec5276438b135a468364edf12",
+    "url": "/static/media/TelaExemplo.88090ace.svg"
+  },
+  {
+    "revision": "3bd921c0d2fac634b2cff66402bb074c",
+    "url": "/static/media/bottom-desktop1.3bd921c0.svg"
+  },
+  {
+    "revision": "6a2a7d2b0d75606c69d2444da37844d4",
+    "url": "/static/media/footer-desktop.6a2a7d2b.svg"
+  },
+  {
+    "revision": "4918e1492526a96242981765fe4321ae",
+    "url": "/static/media/icon-communities.4918e149.svg"
+  },
+  {
+    "revision": "c90d96c2492a2d6f0863256f2bf9015d",
+    "url": "/static/media/icon-email.c90d96c2.svg"
+  },
+  {
+    "revision": "1203b299cf1910559663db88126d869e",
+    "url": "/static/media/icon-messages.1203b299.svg"
+  },
+  {
+    "revision": "4c8c2ba2f1cc804f9092d504c9b0decd",
+    "url": "/static/media/icon-phone.4c8c2ba2.svg"
+  },
+  {
+    "revision": "6641e825f006d463db7028607e3ee994",
+    "url": "/static/media/illustration-flowing-conversation.6641e825.svg"
+  },
+  {
+    "revision": "bb27da81d1984faa5ef57668534ff1a5",
+    "url": "/static/media/illustration-grow-together.bb27da81.svg"
+  },
+  {
+    "revision": "8a5af9952fb5ed23779d589621b108a5",
+    "url": "/static/media/illustration-your-users.8a5af995.svg"
+  },
+  {
+    "revision": "d7906c7efe939da693541fce78c25bfd",
+    "url": "/static/media/logo-facebook.d7906c7e.svg"
+  },
+  {
+    "revision": "50a3fdcf97c39991d665c798fb593f8c",
+    "url": "/static/media/logo-instagram.50a3fdcf.svg"
+  },
+  {
+    "revision": "5e6fb624484d14d1faa1ae24805199ab",
+    "url": "/static/media/logo-twitter.5e6fb624.svg"
+  },
+  {
+    "revision": "3cb867305e954abaa1a8b0dd7a4e7c06",
+    "url": "/static/media/logo.3cb86730.svg"
+  },
+  {
+    "revision": "776264adb030099131b60bda2694a8f5",
+    "url": "/static/media/logoBranco.776264ad.svg"
+  },
+  {
+    "revision": "bd9436af597514dd65ca5d639f1858ba",
+    "url": "/static/media/top-desktop1.bd9436af.svg"
+  }
+]);
